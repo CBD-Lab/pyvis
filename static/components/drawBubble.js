18fac5b410ee8367358354e75e1dc9bb081a7f6f
cwnttj <148994047+cwnttj@users.noreply.github.com>
2023-11-03 15:35:37 +0800

    Update drawBubble.js

--- static/components/drawBubble.js
+++ static/components/drawBubble.js
@@ -11,7 +11,7 @@ function drawBubble(data){
                         .style("stroke-width",2)
                         .style("border-radius", "10px");
     var svg = d3.select("#graph")
-        .attr("width", width)
+        .attr("width", width*0.85)
         .attr("height", height);
 
     var colorrec = svg.selectAll('rect')
@@ -43,8 +43,8 @@ function drawBubble(data){
     					.attr("opacity",0.7)
     					.on("mouseenter", function(event, d) { // "mouseenter" 替代 "mouseover"
 								 d3.select(this)
-								   .attr("stroke", "#000")
-								   .attr("stroke-width", 2);
+								   .attr("stroke", "#555")
+								   .attr("stroke-width", 0.5);
 								tooltip.html(d.data.name)
 											   .style("left", event.pageX + "px")  // 设置X位置为鼠标事件的X坐标
 											   .style("top", event.pageY + "px");  // 设置Y位置为鼠标事件的Y坐标
@@ -52,7 +52,54 @@ function drawBubble(data){
                         .on("mouseleave", function(event, d) { // "mouseleave" 替代 "mouseout"
                             d3.select(this).attr("stroke", null);
                             tooltip.style("visibility",'false');
-                             });
+                             })
+                        .on("click",function(d,i)
+                        {
+                          console.log(d,i)
+                          var fullname = i.data.name.slice(0, -3);;
+                          var point=i;
+                            while(point.depth>=0&& point.parent)
+                            {
+                                point=point.parent;
+                                fullname = point.data.name +'.'+ fullname; // 使用 + 运算符连接字符串
+                            }
+
+                                if(point.data.name=="nn")
+                                fullname="torch."+fullname;
+                                else
+                                fullname=fullname;
+
+                        console.log(d,i,fullname);
+                        fetch('http://127.0.0.1:5006/bubbleCode?wanted=' + fullname)
+                                .then(response => response.text())
+                                .then(data => {
+                                 const language = 'python';
+                             // 使用 Prism.highlight 方法高亮代码字符串
+                                 const highlightedCode = Prism.highlight(data, Prism.languages[language], language);
+                                 var tips = d3.select("body")
+                                                .append("div")
+                                                .attr("class","popup")
+                        
+
+                                tips.append("span")
+                                    .attr("class","close")
+                                    .attr("color","red")
+                                    .text("x")
+                                    .on("click",function(){
+                                    tips.remove();
+                                   });
+
+                                tips.append("div")
+                                    .attr("class","content")
+                                    .html('<pre><code class="language-python">'+highlightedCode+'</code></pre>');
+
+                                    console.log(data);
+                                    })
+                                .catch(error => {
+                                    console.error('Error executing Python script:', error);
+                                    // 处理错误
+                                });
+                        });
 
     }
 
