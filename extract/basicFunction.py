4e9beba3e5501b22e9a1111ef823c8d693305760
suanxiaoyu <1204579157@qq.com>
2023-11-03 11:44:21 +0800

    update

--- extract/basicFunction.py
+++ extract/basicFunction.py
@@ -3,52 +3,54 @@ import ast
 import numpy
 from types import *
 from inspect import isclass
-import torch.nn
-import torchvision
-import torch.onnx
+
 
 def get_modules(wanted):
     modules = inspect.getmembers(wanted, inspect.ismodule)
-    mymodules=[]
+    mymodules = []
     for m in modules:
         mymodules.append(m[1].__name__)
-        #print(m)
+        # print(m)
     return mymodules
 
-#print(get_modules(torch.nn.modules.transformer)) 得到py文件和包 ，多出来的有torch和os和warnings
+
+# print(get_modules(torch.nn.modules.transformer)) 得到py文件和包 ，多出来的有torch和os和warnings
 
 def get_classes(wanted):
     classes = inspect.getmembers(wanted, inspect.isclass)
-    myclasses=[]
+    myclasses = []
     for m in classes:
         myclasses.append(m[1].__name__)
-        #print(m)
+        # print(m)
 
     return myclasses
 
-#print(get_classes(torch.nn.modules.transformer)) 也是init得到类
+
+# print(get_classes(torch.nn.modules.transformer)) 也是init得到类
 
 def get_classes_bydir(wanted):
     classes = [x for x in dir(wanted) if isclass(getattr(wanted, x))]
     return classes
 
-#print(get_classes_bydir(torch.nn.modules.transformer)) 和上面的结果一样
 
-def get_functions(wanted):##to do
+# print(get_classes_bydir(torch.nn.modules.transformer)) 和上面的结果一样
+
+def get_functions(wanted):  ##to do
     funcs = inspect.getmembers(wanted, inspect.isfunction)
-    infuncs=[]
-    outfuncs=[]
+    infuncs = []
+    outfuncs = []
     for m in funcs:
-        if(m[1].__module__ == wanted.__name__):
+        if (m[1].__module__ == wanted.__name__):
             infuncs.append(m[1].__name__)
         else:
             outfuncs.append(m[1].__name__)
-        #print(m)
-    return infuncs,outfuncs
+        # print(m)
+    return infuncs, outfuncs
 
-#print(get_functions(torch.nn.modules.transformer))会直接得到__init__.py里的函数
 
-def internal_classes(wanted):   #classes defined in this .py file
+# print(get_functions(torch.nn.modules.transformer))会直接得到__init__.py里的函数
+
+def internal_classes(wanted):  # classes defined in this .py file
     try:
         src = inspect.getsource(wanted)
         p = ast.parse(src)
@@ -58,18 +60,21 @@ def internal_classes(wanted):   #classes defined in this .py file
         classes = []
     return classes
 
-#print(internal_classes(torch.nn.modules.transformer))直接输入包名跑为空
+
+# print(internal_classes(torch.nn.modules.transformer))直接输入包名跑为空
 
 def in_out_classes_bymodulename(wanted):
     mos = inspect.getmembers(wanted, inspect.isclass)
-    inclasses=[]
-    outclasses=[]
+    inclasses = []
+    outclasses = []
     for m in mos:
-        if(m[1].__module__ == wanted.__name__):
+        if (m[1].__module__ == wanted.__name__):
             inclasses.append(m[1].__module__)
         else:
             outclasses.append(m[1].__module__)
-    return inclasses,outclasses
+    return inclasses, outclasses
+
+
 # ins,outs=in_out_classes_bymodulename(torch.nn.modules.transformer) inclass得到__module__名字和wanted一样的参数的__module__，outclass则是其他的
 # print(ins)
 # print("-------------")
@@ -77,56 +82,18 @@ def in_out_classes_bymodulename(wanted):
 
 
 def get_class_method(wanted):
-    attr=wanted.__dict__
-    #print('attr',attr)
-    cmethod=[]
+    attr = wanted.__dict__
+    # print('attr',attr)
+    cmethod = []
     for item in attr:
-        #print(item)
+        # print(item)
         cmethod.append(item)
     return cmethod
 
-#print(get_class_method(torch.nn.modules.transformer.Transformer)) 得到__init__.py内python的内置函数，import的包和模块，form .xxx的xxx,前面带'_'的参数，以及在同一级目录下的其他模块（包和py文件）
+
+# print(get_class_method(torch.nn.modules.transformer.Transformer)) 得到__init__.py内python的内置函数，import的包和模块，form .xxx的xxx,前面带'_'的参数，以及在同一级目录下的其他模块（包和py文件）
 
 def get_class_attributes(wanted):
     return [item for item in wanted.__dict__ if not callable(getattr(wanted, item)) and not item.startswith('__')]
 
-#print(get_class_attributes(torch.nn.modules)) 还是有开头是'_'的item会被划进来，
-
-if __name__ == "__main__":
-    wanted = torchvision
-    print('torchvision')
-    print(get_modules(wanted))
-    print(get_classes(wanted))
-    print(get_classes_bydir(wanted))
-    print(get_functions(wanted))
-    print(internal_classes(wanted))
-    print(in_out_classes_bymodulename(wanted))
-    print(get_class_method(wanted))
-    print(get_class_attributes(wanted))
-    #print(get_modules(torch.nn.modules.transformer)) 得到py文件和包 和init导入的有torch和os和warnings
-    #print(get_classes(torch.nn.modules.transformer)) 得到类 跑不通
-    #print(get_classes_bydir(torch.nn.modules.transformer)) 跑不通
-    #print(get_functions(torch.nn.modules.transformer))会直接得到__init__.py里的函数
-    #print(internal_classes(torch.nn.modules.transformer))直接输入包名跑为空
-    # ins,outs=in_out_classes_bymodulename(torch.nn.modules.transformer) 直接输入包跑不通，outclass得到modulefinder
-    #print(get_class_method(torch.nn.modules.transformer.Transformer)) 得到__init__.py内python的内置函数，import的包和模块，form .xxx的xxx,前面带'_'的参数，以及在同一级目录下的其他模块（包和py文件）
-    #print(get_class_attributes(torch.nn.modules)) 还是有开头是'_'的item会被划进来，
-
-    wanted = torch.onnx
-    print('torch.onnx')
-    print(get_modules(wanted))
-    print(get_classes(wanted))
-    print(get_classes_bydir(wanted))
-    print(get_functions(wanted))
-    print(internal_classes(wanted))
-    print(in_out_classes_bymodulename(wanted))
-    print(get_class_method(wanted))
-    print(get_class_attributes(wanted))
-    #print(get_modules(torch.nn.modules.transformer)) 主要得到的是init里import的对象
-    #print(get_classes(torch.nn.modules.transformer)) 也是init得到类
-    #print(get_classes_bydir(torch.nn.modules.transformer)) 和上面的结果一样
-    #print(get_functions(torch.nn.modules.transformer))会直接得到__init__.py里的函数 和import的函数
-    #print(internal_classes(torch.nn.modules.transformer))直接输入包名跑为空
-    #ins,outs=in_out_classes_bymodulename(torch.nn.modules.transformer) inclass得到__module__名字和wanted一样的参数的__module__，outclass则是其他的
-    #print(get_class_method(torch.nn.modules.transformer.Transformer)) 得到__init__.py内python的内置函数，import的包和模块，form .xxx的xxx,前面带'_'的参数，init里的定义的函数，以及在同一级目录下的其他模块（包和py文件）
-    #print(get_class_attributes(torch.nn.modules)) 还是有开头是'_'的item会被划进来，
+# print(get_class_attributes(torch.nn.modules)) 还是有开头是'_'的item会被划进来，
--- extract/pyNet4Inspect2ClassFunctionSingle.py
+++ extract/pyNet4Inspect2ClassFunctionSingle.py
@@ -1,7 +1,8 @@
-#剔除了Open3D里的没有文件对应问题（Nodes）
-#剔除了NLTK里别名和重定向问题（Nodes）
+# 剔除了Open3D里的没有文件对应问题（Nodes）
+# 剔除了NLTK里别名和重定向问题（Nodes）
 import argparse
 import inspect
+# from . import basicFunction
 import basicFunction
 import json
 import pathlib
@@ -14,7 +15,13 @@ import glob
 import platform
 import textwrap
 import sys
-#------------------do not delete the import above,using while runtime.----------------------------------
+# import importlib
+# ------------------do not delete the import above,using while runtime.----------------------------------
+import flask
+import torch
+import networkx
+import matplotlib
+from matplotlib import pyplot
 
 # 创建参数解析器
 parser = argparse.ArgumentParser(description="My script")
@@ -32,60 +39,68 @@ links = []
 myclass = ""
 myfunction = ""
 
-layer=0
-def get_modules(pname,initpname,layer):
-    arg=eval(pname)
-    if arg.__name__ ==initpname:
+layer = 0
+
+
+def get_modules(pname, initpname, layer):
+    arg = eval(pname)
+    if arg.__name__ == initpname:
         modules.append(arg.__name__)
-        myclass= basicFunction.in_out_classes_bymodulename(eval(arg.__name__))
-        classcount=len(myclass)
+        myclass = basicFunction.in_out_classes_bymodulename(eval(arg.__name__))
+        classcount = len(myclass)
 
-        myfunction= basicFunction.get_functions(eval(arg.__name__))
-        functioncount=len(myfunction)
-        if("__file__" in dir(eval(arg.__name__))):
-            nodes.append({'name': arg.__name__, 'file': arg.__file__,'ftype':'.py','layer':layer,'hasclass':classcount,'myclass':myclass,"hasfunction":functioncount,"myfunction":myfunction})
+        myfunction = basicFunction.get_functions(eval(arg.__name__))
+        functioncount = len(myfunction)
+        if ("__file__" in dir(eval(arg.__name__))):
+            nodes.append(
+                {'name': arg.__name__, 'file': arg.__file__, 'ftype': '.py', 'layer': layer, 'hasclass': classcount,
+                 'myclass': myclass, "hasfunction": functioncount, "myfunction": myfunction})
         else:
-            nodes.append({'name': arg.__name__, 'file': 'none', 'ftype':'none','layer': layer,
-                                      'hasclass': classcount, 'myclass': myclass, "hasfunction": functioncount,
-                                      "myfunction": myfunction})
-#     count=count+1
+            nodes.append({'name': arg.__name__, 'file': 'none', 'ftype': 'none', 'layer': layer,
+                          'hasclass': classcount, 'myclass': myclass, "hasfunction": functioncount,
+                          "myfunction": myfunction})
+    #     count=count+1
 
-    layer=layer+1
-    mem=inspect.getmembers(eval(pname),inspect.ismodule)
-    for m,m_info in mem:
-        if (pname in m_info.__name__) and not(m_info.__name__ in modules):   #Filter calling external modules
-            if("__file__" in dir(eval(m_info.__name__))):
-                if(pathlib.Path(m_info.__file__).suffix==".py"):
-                    #print("1---.py=",m,m_info.__name__,m_info.__file__)
+    layer = layer + 1
+    mem = inspect.getmembers(eval(pname), inspect.ismodule)
+    for m, m_info in mem:
+        if (pname in m_info.__name__) and not (m_info.__name__ in modules):  # Filter calling external modules
+            print(m_info)
+            print(dir(eval(m_info.__name__)))
+            print(eval(m_info.__name__).__file__)
+            if ("__file__" in dir(eval(m_info.__name__)) and (eval(m_info.__name__).__file__ is not None)):
+                if (pathlib.Path(eval(m_info.__name__).__file__).suffix == ".py"):
+                    # print("1---.py=",m,m_info.__name__,m_info.__file__)
                     modules.append(m_info.__name__)
 
-                    myclass = basicFunction.in_out_classes_bymodulename(eval(m_info.__name__))
-                    classcount = len(myclass)
-
-                    myfunction =basicFunction.get_functions(eval(m_info.__name__))
-                    functioncount = len(myfunction)
-                    nodes.append({'name': m_info.__name__, 'file': eval(m_info.__name__).__file__, 'layer': layer,
-                                      'hasclass': classcount, 'myclass': myclass, "hasfunction": functioncount,
-                                      "myfunction": myfunction})
-
-                    __import__(m_info.__name__)
-
-                else:
-                    #print("2---Not.py(.pyd,.pyi,.pyc)",m,m_info.__name__,m_info.__file__)
-                    modules.append(m_info.__name__)
-                    ex=os.path.splitext(m_info.__file__)[1]
-                    #print(ex)
                     myclass = basicFunction.in_out_classes_bymodulename(eval(m_info.__name__))
                     classcount = len(myclass)
 
                     myfunction = basicFunction.get_functions(eval(m_info.__name__))
                     functioncount = len(myfunction)
-                    nodes.append({'name': m_info.__name__, 'file': eval(m_info.__name__).__file__, 'ftype':ex,'layer': layer,
-                                      'hasclass': classcount, 'myclass': myclass, "hasfunction": functioncount,
-                                      "myfunction": myfunction})
+                    nodes.append({'name': m_info.__name__, 'file': eval(m_info.__name__).__file__, 'layer': layer,
+                                  'hasclass': classcount, 'myclass': myclass, "hasfunction": functioncount,
+                                  "myfunction": myfunction})
+
+                    # __import__(m_info.__name__)
+
+                else:
+                    # print("2---Not.py(.pyd,.pyi,.pyc)",m,m_info.__name__,m_info.__file__)
+                    modules.append(m_info.__name__)
+                    ex = os.path.splitext(m_info.__file__)[1]
+                    # print(ex)
+                    myclass = basicFunction.in_out_classes_bymodulename(eval(m_info.__name__))
+                    classcount = len(myclass)
+
+                    myfunction = basicFunction.get_functions(eval(m_info.__name__))
+                    functioncount = len(myfunction)
+                    nodes.append(
+                        {'name': m_info.__name__, 'file': eval(m_info.__name__).__file__, 'ftype': ex, 'layer': layer,
+                         'hasclass': classcount, 'myclass': myclass, "hasfunction": functioncount,
+                         "myfunction": myfunction})
 
             else:
-                #print("3---NoFile",m,m_info.__name__)
+                # print("3---NoFile",m,m_info.__name__)
                 modules.append(m_info.__name__)
 
                 myclass = basicFunction.in_out_classes_bymodulename(eval(m_info.__name__))
@@ -93,21 +108,21 @@ def get_modules(pname,initpname,layer):
 
                 myfunction = basicFunction.get_functions(eval(m_info.__name__))
                 functioncount = len(myfunction)
-                nodes.append({'name': m_info.__name__, 'file': 'none', 'ftype':'none','layer': layer,
-                                  'hasclass': classcount, 'myclass': myclass, "hasfunction": functioncount,
-                                  "myfunction": myfunction})
+                nodes.append({'name': m_info.__name__, 'file': 'none', 'ftype': 'none', 'layer': layer,
+                              'hasclass': classcount, 'myclass': myclass, "hasfunction": functioncount,
+                              "myfunction": myfunction})
             if ("__file__" in dir(eval(m_info.__name__))):
                 get_modules(m_info.__name__, initpname, layer)
     return modules
 
 
-def get_links(mymodule,pname):
-    i=0
+def get_links(mymodule, pname):
+    i = 0
     for m in mymodule:
         nextmodules = []
-        print(i,"=",m)
-        i=i+1
-        try:   #torch.classes can not use inspect.getmembers(torch.classes)
+        print(i, "=", m)
+        i = i + 1
+        try:  # torch.classes can not use inspect.getmembers(torch.classes)
             mm = inspect.getmembers(eval(m), inspect.ismodule)
 
             for (name, moduleurl) in mm:
@@ -124,34 +139,43 @@ def get_links(mymodule,pname):
     return links
 
 
+def netjson(filename, initpname):
+    # 递归搜索模块Module节点
 
-def netjson(filename):
-    #递归搜索模块Module节点
-
-    mymodule = get_modules(filename,initpname,layer)
+    mymodule = get_modules(filename, initpname, layer)
 
     print(len(nodes))
-    get_links(mymodule,initpname)
+    get_links(mymodule, initpname)
 
     mnetjson['nodes'] = nodes
     mnetjson['links'] = links
-#------------------------ single test using the 137th line --------------------------
+    # ------------------------ single test using the 152th line --------------------------
     # f = open('../static/netjson/'+filename+'.json', 'w')
     f = open('static/netjson/' + filename + '.json', 'w')
     f.write(json.dumps(mnetjson))
     f.close()
 
-#-------------------------------filename = '' is triggered in html page, if test in this .py use 144th line--------------------------------------------
-#path = r'D:\ProgramData\Anaconda3\Lib\site-packages'
-# path=r'H:\PyVisVue3D3V7\venv\Lib\site-packages'
-# filename = 'numpy'
-initpname=filename
-from matplotlib import pyplot
 
-import_statement = "import " + filename
+# #-------------------------------filename = '' is triggered in html page, if test in this .py use 146th line--------------------------------------------
+# #path = r'D:\ProgramData\Anaconda3\Lib\site-packages'
+# # path=r'H:\PyVisVue3D3V7\venv\Lib\site-packages'
+# #filename = 'networkx'
+# initpname=filename
+# from matplotlib import pyplot
+# import_statement = "import " + filename
+# print(import_statement)
+# exec(import_statement)
+#
+# #__import__(filename)
+# # #
+# netjson(filename)
+
+
+moduleName = filename
+import_statement = "import " + moduleName
+initpname = moduleName
 print(import_statement)
 exec(import_statement)
-import torch
-# __import__(filename)
-# #
-netjson(filename)
+# __import__(moduleName)
+# importlib.import_module(moduleName)
+netjson(moduleName, initpname)
