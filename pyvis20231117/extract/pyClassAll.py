44b3e55c902ec530cd246ae49dd45da60693c154
suanxiaoyu <1204579157@qq.com>
2023-11-17 17:02:20 +0800

    代码整合更新

--- pyvis20231117/extract/pyClassAll.py
+++ pyvis20231117/extract/pyClassAll.py
@@ -2,12 +2,14 @@ import os
 import pathlib
 import json
 from extract import basicFunction
+import builtins
 
 
 def get_classes(path, pname):
     exec("import " + pname)
     print(path)
     lsdir = os.listdir(path)
+    print(lsdir[0:3])
     for f in lsdir:
         if (f != '__pycache__') and (f != 'test') and (f != 'testing'):
             if os.path.isfile(os.path.join(path, f)):
@@ -15,35 +17,68 @@ def get_classes(path, pname):
                     modulepath = os.path.splitext(os.path.join(path, f))[0]
                     modulepath = modulepath[modulepath.find(r"site-packages") + 14:len(modulepath)]
                     modulepath = modulepath.replace('\\', '.')
+                    print("-----------" + modulepath)
                     # import_statement = "import " + modulepath
                     # exec(import_statement)
                     # print(modulepath) #torch.nn.modules.transformer
                     try:
-                        inclass, _ = basicFunction.in_out_classes_bymodulename(eval(modulepath))
-
+                        inclass, inclasspath, _ = basicFunction.in_out_classes_bymodulename4path(eval(modulepath))
+                        print("---------2---------")
+                        print(inclasspath)
                         for c in inclass:
                             exec("from" + " " + modulepath + " " + "import" + " " + c)
                             methods, attributes = basicFunction.get_class_method_attr(eval(c))
-                            parent = eval(c).__bases__[0].__name__
-                            instantiateOtherClasses = basicFunction.InstantiateOtherClasses(eval(c))
-                            tmpclass = {"name": c, "myparent": parent, "methods": methods, "attributes": attributes,"otherclasseslink":instantiateOtherClasses}
-                            myclasses.append(c)
+                            # parent = eval(c).__bases__[0].__name__
+                            parent = eval(inclasspath[inclass.index(c)]).__bases__
+                            if (len(parent)) > 1:
+                                print(">1----------------------")
+                                parentname = ""
+                                for p in parent:
+                                    parentname = parentname + p.__module__ + "." + p.__name__ + "|"
+                                parentname = parentname[0:-2]
+                            else:
+                                print("=1  Parent Class is 1 ----------------------")
+                                parentname = parent[0].__module__ + "." + parent[0].__name__
+                            instantiateOtherClasses = basicFunction.InstantiateOtherClasses(
+                                inclasspath[inclass.index(c)])
+                            tmpclass = {"name": inclasspath[inclass.index(c)], "myparent": parentname,
+                                        "methods": methods, "attributes": attributes,
+                                        "otherclasseslink": instantiateOtherClasses}
+                            myclasses.append(inclasspath[inclass.index(c)])
                             nodes.append(tmpclass)
                             # print(classes)
-                    except:
-                        pass
+                    except Exception as error:
+                        print(error)
     dirs = [i for i in lsdir if os.path.isdir(os.path.join(path, i))]
     if dirs:
         for i in dirs:
             get_classes(os.path.join(path, i), pname)
 
+    return myclasses
+
 
 def get_links(myclasses, nodes):
+    print("----------myclasses------")
+    print(myclasses)
+    print("---------------nodes-------------")
+    print(nodes)
     for node in nodes:
-        myparent = node['myparent']
-        myname = node['name']
-        if str(myparent) in myclasses:
-            links.append({'source': myclasses.index(str(myparent)), 'target': myclasses.index(myname)})
+        myparent = node['myparent'].split("|")
+        print(myparent)
+
+        for p in myparent:
+            print("-----p--------")
+            print(p)
+            if p != "":
+                myname = node['name']
+                if p in myclasses:
+                    print("link")
+                    links.append({'source': myclasses.index(myname), 'target': myclasses.index(p)})
+                else:
+                    myclasses.append(p)
+                    nodes.append({"name": p, "myparent": ""})
+                    links.append({'source': myclasses.index(myname), 'target': myclasses.index(p)})
+    return links
 
 
 def readpackages():
---
+++ pyvis20231117/static/components/drawBubble.js
@@ -0,0 +1,98 @@
+function drawBubble(data) {
+      let tooltip = d3.select('body')
+        .append('div')
+        .attr("id", "tip")
+        .attr("class", "tooltips");
+
+      var svg = d3.select("#graph")
+        .attr("width", width * 0.85)
+        .attr("height", height);
+
+      var colorrec = svg.selectAll('rect')
+        .data(color)
+        .enter()
+        .append("rect")
+        .attr("x", (d, i) => (i * 16 + width * 0.8))
+        .attr("y", 20)
+        .attr("width", 14)
+        .attr("height", 14)
+        .attr("fill", (d, i) => color[i]);
+
+      var pack = d3.pack()
+        .size([width, height]);
+      var hidata = d3.hierarchy(data, d => d.children)
+        .sum(d => d.value);
+      var packdata = pack(hidata);
+      var nodes = packdata.descendants();
+      var gc = svg.selectAll("g")
+        .data(nodes)
+        .enter()
+        .append("g")
+        .attr("transform", d => `translate(${d.x},${d.y})`);
+      var circles = gc.append("circle")
+        .attr("cx", 0)
+        .attr("cy", 0)
+        .attr("r", d => d.r)
+        .attr("fill", d => color[d.depth])
+        .attr("opacity", 0.7)
+        .style("cursor", "pointer")
+        .on("mouseenter", (event, d) => {
+          d3.select(this.d)
+            .attr("stroke", "#555")
+            .attr("stroke-width", 0.5);
+          tooltip.html(d.data.name)
+                 .style("left", event.pageX + "px")
+                 .style("top", event.pageY + "px");
+        })
+        .on("mouseleave", (event, d) => {
+          d3.select(this.d).attr("stroke", null);
+          tooltip.style("visibility", 'false');
+        })
+        .on("click", (d, i) => {
+          console.log(d, i);
+          var fullname = i.data.name.split('.', 1)[0];
+          var point = i;
+          while (point.depth >= 0 && point.parent) {
+            point = point.parent;
+            fullname = point.data.name + '.' + fullname;
+          }
+
+          if (point.data.name == "nn")
+            fullname = "torch." + fullname;
+          else
+            fullname = fullname;
+
+          console.log(d, i, fullname);
+          fetch('http://127.0.0.1:5006/leafCode?wanted=' + fullname)
+            .then(response => response.text())
+            .then(data => {
+              const language = 'python';
+              const highlightedCode = Prism.highlight(data, Prism.languages[language], language);
+              var tips = d3.select("body")
+                           .append("div")
+                           .attr("class", "popup");
+
+              tips.append("span")
+                  .attr("class", "close")
+                  .attr("color", "red")
+                  .text("x")
+                  .on("click", () => {
+                         tips.remove();
+                });
+
+              tips.append("div")
+                .attr("class", "content")
+                .html('<pre><code class="language-python">' + highlightedCode + '</code></pre>');
+
+              console.log(data);
+            })
+            .catch(error => {
+              console.error('Error executing Python script:', error);
+            });
+        });
+}
+
+window.onDrawBubbleReady = data => {
+  // 执行绘图逻辑
+  drawBubble(data);
+};
